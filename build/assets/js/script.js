1ed70de79cc21a010101de66469db2737b794fd4
Matt Cass <matthewcasserly@gmail.com>
2015-03-06 17:07:13 -0500

    starting js

--- build/assets/js/script.js
+++ build/assets/js/script.js
@@ -2,6 +2,13 @@
 
 (function() {
 
-	console.log('this is an test of the emergency alert system')
+	console.log('this is an test of the emergency alert system');
+
+		var amex = $('#amex');
+		var csv  = $('.csv');
+		
+		(amex).on('click', function() {
+			(csv).toggleClass('xpress');
+		});
 
 })();
